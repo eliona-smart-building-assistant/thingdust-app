11c3af6a624357fc6b5030cbca91a891e7d6455d
airvine-leicom <124673256+airvine-leicom@users.noreply.github.com>
2023-02-09 14:05:00 +0100

    Initial commit

---
+++ app.go
@@ -0,0 +1,41 @@
+//  This file is part of the eliona project.
+//  Copyright © 2022 LEICOM iTEC AG. All Rights Reserved.
+//  ______ _ _
+// |  ____| (_)
+// | |__  | |_  ___  _ __   __ _
+// |  __| | | |/ _ \| '_ \ / _` |
+// | |____| | | (_) | | | | (_| |
+// |______|_|_|\___/|_| |_|\__,_|
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package main
+
+import (
+	"github.com/eliona-smart-building-assistant/go-utils/common"
+	"github.com/eliona-smart-building-assistant/go-utils/http"
+	"github.com/eliona-smart-building-assistant/go-utils/log"
+	nethttp "net/http"
+	"template/apiserver"
+	"template/apiservices"
+)
+
+// doAnything is the main app function which is called periodically
+func doAnything() {
+
+	// Todo: implement everything the app should do
+	log.Debug("main", "do anything")
+
+}
+
+// listenApi starts the API server and listen for requests
+func listenApi() {
+	http.ListenApiWithOs(&nethttp.Server{Addr: ":" + common.Getenv("API_SERVER_PORT", "3000"), Handler: apiserver.NewRouter(
+		apiserver.NewConfigurationApiController(apiservices.NewConfigurationApiService()),
+		apiserver.NewVersionApiController(apiservices.NewVersionApiService()),
+	)})
+}
