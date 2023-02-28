5635f8f96f6560aedef46eb08a3159ceb2dd8ff6
andrew-leicom <andrew.irvine@leicom.ch>
2023-02-28 10:29:38 +0100

    Continuous Asset Creation implemented

--- app.go
+++ app.go
@@ -16,69 +16,80 @@
 package main
 
 import (
-	//"fmt"
 	"context"
 	nethttp "net/http"
-
 	"thingdust/apiserver"
 	"thingdust/apiservices"
 	"thingdust/conf"
-
-	//"os"
-
+	"thingdust/thingdust"
+	"github.com/eliona-smart-building-assistant/go-eliona/asset"
 	"github.com/eliona-smart-building-assistant/go-utils/common"
 	"github.com/eliona-smart-building-assistant/go-utils/http"
-
-	//"encoding/json"
-
 	"github.com/eliona-smart-building-assistant/go-utils/log"
-
-	//"io/ioutil"
+	"thingdust/eliona"
 	"time"
+	//api "github.com/eliona-smart-building-assistant/go-eliona-api-client/v2"
 )
 
-type Spaces map[string]Space
-
-
-type Space struct {
-	Humidity float64 `json:"humidity"`
-	Occupancy string `json:"occupancy"`
-	Temperature float64 `json:"temperature"`
-}
 
 // doAnything is the main app function which is called periodically
 func doAnything() {
-
-
+	// Fetch Spacs using thingdust api
 	request, err := http.NewRequestWithApiKey("https://demo.cust.prod.thingdust.io/api/v2/get_space_states", "X-API-KEY", "UEKNEYKACORWF9JMYBGLPOCPIBHNJUHYIAADBRQCEHQM2V7YJUSCVBFUNOWW" )
-	
 	if err != nil {
 		log.Error("spaces", "Error with request: %v", err)
 		return
 	}
-
-	spaces, err1 := http.Read[Spaces](request, time.Duration(time.Duration.Seconds(1)), true)
-
+	spaces, err1 := http.Read[thingdust.Spaces](request, time.Duration(time.Duration.Seconds(1)), true)
 	if err1 != nil {
 		log.Error("spaces", "Error reading spaces: %v",err1)
 		return
 	}
-	log.Debug("spaces", "Read %d spaces", len(spaces))
-
 	for spaceName:= range spaces {
 		// Mapping exists?
-		confSpace, err2 :=conf.GetSpace(context.Background(), 1, "", spaceName)
+		confSpace, err2 :=conf.GetSpace(context.Background(), 1, "empty", spaceName)
 		if err2 != nil {
 			log.Error("spaces", "Error when reading spaces from configurations")
 			return
 		}
-
 		if confSpace == nil {
-			// Create Asset Mapping
-		}
-		else{
+			// Create Asset Mapping between newly generated asset and space with key spaceName
+			assetId, err := eliona.CreateNewAsset("empty", spaceName)
+			if err != nil {
+				log.Error("spaces", "Error when creating new asset")
+				return
+			}
+			if err == nil {
+				log.Debug("spaces", "AssetId %v assigned to space %v", assetId, spaceName)
+			}
+			err = conf.InsertSpace(context.Background(), 1, "empty", spaceName, assetId)
+			if err != nil {
+				log.Error("spaces","Error when inserting space into database")
+				return
+			}
+		} else {
 			//Asset exists
+			exists, err := asset.ExistAsset(confSpace.AssetId)
+			if err != nil {
+				log.Error("spaces","Error when checking if asset already exists")
+				return
+			}
+			if exists {
+				log.Debug("spaces", "Asset already exists for space %v with AssetId %v", spaceName, confSpace.AssetId)
+			}else {
+				continue
+			}
+			
+
 		}
+		//  asset.UpsertData(api.Data{
+		// 	AssetId: confSpace.AssetId,
+		// 	Subtype: "input",
+		// 	Data: spaces[spaceName].(thingdust.Space),
+		// 	AssetTypeName: *api.NewNullableString(common.Ptr("thingdust_space")),
+		//  })
+
+		// // })
 	}
 
 }
