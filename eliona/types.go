7a7d34fc1e78d03ee5c4f9e2ebf133ed1db917c4
andrew-leicom <andrew.irvine@leicom.ch>
2023-03-03 13:36:35 +0100

    Change attribute name back to occupancy

--- eliona/types.go
+++ eliona/types.go
@@ -1,13 +1,9 @@
 package eliona
 
-import (
-
-)
-
 
 
 type Data struct {
 	Humidity float64 `json:"humidity"`
-	Occupancy bool`json:"occupied"`
+	Occupancy bool`json:"occupancy"`
 	Temperature float64 `json:"temperature"`
 }
