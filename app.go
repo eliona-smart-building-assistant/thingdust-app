aae3aaf216c87c896c18ca39e52d978db31f9835
andrew-leicom <andrew.irvine@leicom.ch>
2023-03-30 10:11:41 +0200

    App exits nicely on ctrl C

--- app.go
+++ app.go
@@ -206,3 +206,11 @@ func listenApi() {
 		apiserver.NewSpacesApiController(apiservices.NewSpacesApiService()),
 	)})
 }
+func listenApiRequests() {
+	err := nethttp.ListenAndServe(":"+common.Getenv("API_SERVER_PORT", "3000"), apiserver.NewRouter(
+		apiserver.NewConfigurationApiController(apiservices.NewConfigurationApiService()),
+		apiserver.NewVersionApiController(apiservices.NewVersionApiService()),
+		apiserver.NewCustomizationApiController(apiservices.NewCustomizationApiService()),
+	))
+	log.Fatal("main", "Error in API Server: %v", err)
+}
